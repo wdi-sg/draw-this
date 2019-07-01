a367fa358b32acba1b6580fc1ff9bfde8a5b894a
MalcolmLow83 <malcomlow@gmail.com>
2019-07-02 00:12:46 +0800

    part 1 of first version

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-console.log("hello script js");
+var inputArray = [];
+var showArray = [];
+
+var i = 0;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    document.getElementById("input").value = "";
+    console.log( currentInput );
+    inputArray.push(currentInput);
+    console.log("inputArray: " + inputArray);
+
+    for (i = 0; i < currentInput; i++) {
+            console.log('i: ' + i)
+            showArray.push("🍍");
+            console.log('showArray: ' + showArray)
+    }
+    display();
+
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+var display = function(){
+  document.getElementById("output").innerHTML = showArray;
 };
+
+// console.log("hello script js");
+
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+//   display( "WOW SOMETHING HAPPENED" );
+// };
+
+// var display = function(stuffToDisplay){
+//   // your DOM manipulation code here
+
+// };
