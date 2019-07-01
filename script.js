78f0f522f2b0abd21403c68cf52e9f6b44818759
Yew <limtheanyew@gmail.com>
2019-07-01 21:24:51 +0800

    First Version Complete

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  //display( "WOW SOMETHING HAPPENED" );
+  if(currentInput == "clear"){
+      clearDisplay();
+  }
+  else{
+    display(currentInput);
+  }
 };
 
-var display = function(stuffToDisplay){
+var display = function(stuffToDisplay) {
   // your DOM manipulation code here
-  
+  var outputString = "";
+  for (var i = 0; i < Number(stuffToDisplay); i++) {
+    outputString += "\🍍";
+  }
+
+  var p = document.createElement("p");
+  p.textContent = outputString;
+  console.log(outputString);
+  document.getElementById("output").appendChild(p);
+};
+var clearDisplay = function(){
+    document.getElementById("output").innerHTML = "";
 };
