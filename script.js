41d3c51c78d661998debda9f2876b0724a1824cd
EliseChia <swedesurrender@gmail.com>
2019-07-01 19:35:11 +0800

    Moving on to second version

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var pineappleContainer = document.createElement("p");
+// var pineappleContainer = document.createElement("p");
 
 var inputBox = document.getElementById("input");
 var outputBox = document.getElementById("output");
 
 
-// Enter to submit. not sure if this will work or the assigning to variable must be a function
 inputBox.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     addPineapples();
   }
 });
 
-// var pineapples = [];
-
-var addPineapples = function () {
+var addMorePineapples = function() {
     var inputVal = inputBox.value;
+    var pineappleContainer = document.createElement("p");
     pineappleContainer.textContent = "";
         for (i = 0; i < inputVal; i++) {
             pineappleContainer.textContent += '🍍';
         }
-    // document.body.appendChild(pineappleContainer);
     outputBox.appendChild(pineappleContainer);
+};
+
+var addPineapples = function () {
+    var piney = inputBox.value;
+    if (piney === "clear") {
+        location.reload();
+    } else {
+    addMorePineapples();
+    }
 };
