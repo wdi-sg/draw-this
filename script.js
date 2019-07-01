2b1ab77aa48508120639d4fef085c094b1092f56
emijay <hilmi.johari@yahoo.com.sg>
2019-07-01 22:52:53 +0800

    Created first version of game logic

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
-console.log("hello script js");
+// console.log("hello script js");
 
+// Declaring global variables
+let pineappleArray = [];
+let newPineappleString;
+let newParagraph = document.createElement("p");
+let documentBody = document.body;
+
+
+// When user types in an input
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+
+    // To reset everything back to zero
+    newParagraph = document.createElement("p");
+    pineappleArray = [];
+
+
+    // ParseInt the user's input
+    let userInput = parseInt(currentInput);
+
+    // To create the string of pineapples in <p></p> based on user's input
+    createPineappleArray(userInput);
+
+    newParagraph.textContent = newPineappleString;
+
+    documentBody.appendChild(newParagraph);
+
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+
+const createPineappleArray = function(userInput) {
+
+    for (let i = 0; i < userInput; i ++) {
+        pineappleArray.push("🍍");
+    }
+
+    newPineappleString = pineappleArray.join("")
+
+}
+
+
+
+
+
+
+
+
+
+// var display = function(stuffToDisplay){
+//     // your DOM manipulation code here
+
+//     let outputBox = document.getElementById("output")
+
+//     outputBox.textContent = stuffToDisplay;
+
+// };
