c1d7fbddf3560ae0dc66d7e09682ec3a288b298a
Yew <limtheanyew@gmail.com>
2019-07-02 00:34:09 +0800

    Revert "Revert "Done parts of second version""
    This reverts commit 5aacc88b25398d1054718a356abe1c99ca4a48d0.

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  //display( "WOW SOMETHING HAPPENED" );
-  if(currentInput == "clear"){
-      clearDisplay();
-  }
-  else{
-    display(currentInput);
-  }
+var delayTimer;
+
+var charDraw = prompt("Which Emoji or Character would you like to use?") + " ";
+if (charDraw == " "){
+    charDraw = "🍍";
 };
 
+var inputHappened = function(currentInput) {
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function() {
+      console.log(currentInput);
+  var arrCurrentInput = currentInput.split(" ");
+  if (arrCurrentInput[0] == "clear" && typeof Number(arrCurrentInput[1]) == "number") {
+    clearRow(Number(arrCurrentInput[1]) - 1);
+  } else if (arrCurrentInput.length == 1) {
+    display(currentInput);
+  } else if (arrCurrentInput[0] == "triangle") {
+    displayTriangle(arrCurrentInput[1]);
+         } else {
+           displayTwoDim(arrCurrentInput[0], arrCurrentInput[1] - 1);
+         }
+  //console.log(arrCurrentInput[1]); //For Debugging
+    }, 1000);
+
+};
+
+var outputElement = document.getElementById("output");
+
 var display = function(stuffToDisplay) {
-  // your DOM manipulation code here
+  // DOM manipulation code here
   var outputString = "";
   for (var i = 0; i < Number(stuffToDisplay); i++) {
-    outputString += "\🍍";
+    outputString += charDraw;
   }
-
   var p = document.createElement("p");
   p.textContent = outputString;
   console.log(outputString);
-  document.getElementById("output").appendChild(p);
+  outputElement.appendChild(p);
 };
-var clearDisplay = function(){
-    document.getElementById("output").innerHTML = "";
+var displayTwoDim = function(x, y) {
+  for (var i = 0; i < y; i++) {
+    display(x);
+  }
+};
+var displayTriangle = function(size){
+    for(var i = 1; i <= size ; i ++){
+        display(size);
+    }
+};
+var clearDisplay = function() {
+  document.getElementById("output").innerHTML = "";
+};
+var clearRow = function(rowNum) {
+  outputElement.removeChild(outputElement.children[rowNum]);
 };
