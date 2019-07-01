4af1594e4db07355cb0afba09211e21312d58ccf
kach92 <kenny.ang9206@gmail.com>
2019-07-01 19:46:04 +0800

    do further task to enter multiple rows. can specify as many rows as u want in a single input. will still run if end of input has some strings. only the numbers will be executed

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 console.log("hello script js");
-
+console.log(parseInt("3 2"))
 
 
 
 var inputHappened = function(currentInput){
-    var parsedInput = parseInt(currentInput);
     var arr = null;
-    if(!isNaN(parsedInput)){
-        display( "🍍".repeat(parsedInput));
-        displayError(" ");
-    }else if (isNaN(parsedInput)){
+
+    if(currentInput.includes(" ")){
         arr = currentInput.split(" ");
         if(arr[0]=== "clear"){
             if(!isNaN(parseInt(arr[1]))){
                 clearDisplay(parseInt(arr[1]));
                 displayError(" ");
             }
-            else
+            else{
                 displayError();
-        }else
+            }
+        }else if(!isNaN(parseInt(arr[0]))){
+            display(parseInt(arr[0]));
+            for(i=1;i<arr.length;i++){
+                if(!isNaN(parseInt(arr[i])))
+                    display(parseInt(arr[i]));
+                else
+                    break;
+            }
+        }else{
             displayError();
+        }
+    }else{
+        if(!isNaN(parseInt(currentInput))){
+            display(parseInt(currentInput));
+            displayError(" ");
+        }else{
+            displayError();
+        }
+
     }
+}
 
 
 
-};
+
+
 
 //to add p tag into output box
-var display = function(stuffToDisplay){
+var display = function(num){
   // your DOM manipulation code here
   var pElement = document.createElement("p");
   pElement.setAttribute("class","for-output")
-  pElement.textContent = stuffToDisplay;
+  pElement.textContent = "🍍".repeat(num);
   var temp = document.getElementById("output");
   temp.appendChild(pElement);
 };
