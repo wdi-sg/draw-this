f2273d2e869c1f0d73dfd6010ca8334d36bd00c0
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-07-01 19:06:28 +0800

    first commit. part 1 - draw fruits

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 console.log("hello script js");
 
+var numInput = 0;
+var fruitsArray = [];
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
   display( "WOW SOMETHING HAPPENED" );
 };
 
+document.getElementById('javascriptButton').onclick = function interpretAnswer() {
+  var inputResults = document.getElementById('input').value;
+  console.log("input value: " + inputResults);
+      parsedInput = inputResults.toLowerCase();
+      numInput = parseInt(inputResults);
+    console.log("Parsed input value: " + parsedInput + " or " + numInput);
+    checkAnswer();
+};
+
+
+
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+    var output = document.querySelector('#output');
+    output.innerText = data;
+};
+
+var drawFruits = function() {
+    console.log('drawing fruits');
+    var drawRow = document.createElement('p');
+    for (let i = 0; i < numInput; i++) {
+    drawRow.textContent += "🍍";
+    }
+    console.log(drawRow);
+    output.appendChild(drawRow);
+
+};
+
+function checkAnswer() {
+    drawFruits();
 };
