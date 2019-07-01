3b8092cdc12b965e729330c14ff957663864519c
wilfredloh <lohwilfred19@gmail.com>
2019-07-01 23:00:28 +0800

    1st feature done. now onto 2nd version.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    let input;
+
+    // if (currentInput === 'clear'){
+    //     clear all fields
+    // } else {
+    //     input = parseInt(currentInput);
+    // }
+
+    input = parseInt(currentInput);
+
+    display(input);
+    event.target.value = '';
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+var display = function(input){
+    let output = document.getElementById('output');
+    let newPar = document.createElement("p");
+    let pineapple = '';
+
+    for (let i=0; i<input; i++){
+        pineapple += '🍍';
+    }
+    newPar.textContent = pineapple;
+    output.appendChild(newPar);
 };
