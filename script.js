5a52007911c411c3a34190e777c2f09bf615eb1b
aliciawongg <w.alicia@gmail.com>
2019-07-01 19:34:17 +0800

    completed first version

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 console.log("hello script js");
+console.log('🍍');
+
+var num = [];
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  num = currentInput.split();
+  console.log(num);
+  display();
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+    var fruits = document.createElement("p");
+    for (let i = 0; i < num; i++ ) {
+        fruits.innerHTML += '🍍';
+    };
+    console.log (fruits);
+    var box = document.getElementById("output");
+    box.appendChild(fruits);
 };
