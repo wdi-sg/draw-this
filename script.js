8f983458df2125630e30fd5c401061c368211550
sowyuen <sowyuenl@gmail.com>
2019-07-02 00:52:51 +0800

    done till "type clear to clear the elements out of the DOM."

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 console.log("hello script js");
+var numRecord = [];
 
-var inputHappened = function(currentInput){
+function inputHappened(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+  var input = parseInt(currentInput);
+  numRecord.push(currentInput);
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+  var newP = document.createElement("p");
+  var pineapple = [];
+  var i = 0;
+  for (i=0;i<input;i++){
+    pineapple.push('🍍');
+  }
+  newP.innerHTML = pineapple.join("");
+  document.getElementById('output').append(newP);
+  if (currentInput === "clear"){
+    var output = "";
+    display(output);
+      }
 };
