d137239ba7efd676a3cfa9f588092ae2bea8a3c3
gitwenlei <ngwenlei@gmail.com>
2019-07-02 00:43:35 +0800

    Complete task for program draws a new row, adding to what's already there.

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 console.log("hello script js");
 
-var userInput;
-
-// CREATE A TXT ARRAY TO STORE NO. OF PINEAPPLES TO DISPLAY
-var textInside = [];
-var row;
-
 var inputHappened = function(currentInput){
-    // GENERATE NO. OF PINEAPPLES BASED ON USER INPUT NUMBER
+    // PARSED USER INPUT INTO INTEGER
+    var userInput = parseInt(currentInput);
+    console.log("User enters: " + userInput);
+
+    // CREATE A NEW ARRAY TO STORE USER INPUT OVER TIME
+    var textInside = [];
+
     for (var i = 0; i < currentInput; i++) {
-        // PUSH NEW PINEAPPLE INTO ARRAY
+        // PUSH NEW PINEAPPLE INTO ARRAY BASED ON USER INPUT NUMBER
         textInside.push("🍍");
     }
-  display(textInside);
+    // DISPLAY THE ARRAY OF PINEAPPLES
+    display(textInside);
 };
 
 var display = function(data) {
+    // RETURN THE 1ST ELEMENT WITH ID #OUTPUT & STORE IT INSIDE VARIABLE
     var output = document.querySelector('#output');
-    //var row = data.join(" ");
-    row = data.join(" ");
-    output.innerHTML = row;
+
+    // CREATE NEW EMPTY <P> ELEMENT
+    var createP = document.createElement('p');
+
+    // PUT CONTENT FROM PINEAPPLE TEXTINSIDE ARRAY AND MAKE IT THE INNER-HTML OF THE NEWLY CREATED <P> TAG
+    createP.innerHTML = data.join(' ');
+
+    // APPEND THE NEWLY CREATED P CONTENT AS THE LAST CHILD OF #OUTPUT
+    document.getElementById('output').appendChild(createP);
 
 };
