3e9a29df174d6040732c86d58eb7538fe6ec00f7
shirleytwl <shirtanwl@gmail.com>
2019-07-01 21:55:08 +0800

    first version

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    if (currentInput === "clear"){
+
+    }
+    else {
+        let numOfPineapples = parseInt(currentInput.trim());
+        if (!isNaN(numOfPineapples)) {
+            let pineappleLine = document.createElement("div");
+            for (var i=0;i<numOfPineapples;i++){
+                let pineapple = document.createElement("span");
+                pineapple.innerText = "🍍";
+                pineappleLine.appendChild(pineapple);
+            }
+            display(pineappleLine);
+            displayError("");
+        }
+        else {
+            displayError("Please type in a valid number!");
+        }
+    }
 };
 
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+    document.getElementById("output").appendChild(stuffToDisplay);
 };
+
+var displayError = (data) => {
+    let errorDisplay = document.querySelector('#error');
+    errorDisplay.innerText = data;
+}
