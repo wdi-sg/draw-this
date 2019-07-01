55803e8697bfceddeae487f63859de7517e87812
EliseChia <swedesurrender@gmail.com>
2019-07-01 19:07:00 +0800

    First part complete

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-console.log("hello script js");
+var pineappleContainer = document.createElement("p");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+var inputBox = document.getElementById("input");
+var outputBox = document.getElementById("output");
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+
+// Enter to submit. not sure if this will work or the assigning to variable must be a function
+inputBox.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addPineapples();
+  }
+});
+
+// var pineapples = [];
+
+var addPineapples = function () {
+    var inputVal = inputBox.value;
+    pineappleContainer.textContent = "";
+        for (i = 0; i < inputVal; i++) {
+            pineappleContainer.textContent += '🍍';
+        }
+    // document.body.appendChild(pineappleContainer);
+    outputBox.appendChild(pineappleContainer);
 };
