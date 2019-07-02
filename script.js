389b2721b1a7d8ff704d037518be37ad7a4a8b44
wilfredloh <lohwilfred19@gmail.com>
2019-07-02 08:56:26 +0800

    tried to do extra but messed up my code. the program doesn't run now...

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-    let input;
-
-    // if (currentInput === 'clear'){
-    //     clear all fields
-    // } else {
-    //     input = parseInt(currentInput);
-    // }
-
-    input = parseInt(currentInput);
-
-    display(input);
+    display(currentInput);
     event.target.value = '';
 };
 
 var display = function(input){
+    let wordArray = input.split(" ");
+        console.log('wordArray length:'+ wordArray.length)
+        console.log('wordArray:'+ wordArray)
+
     let output = document.getElementById('output');
     let newPar = document.createElement("p");
+    let parsedInputZero;
+    let parsedInputOne;
+    let childCount = output.childElementCount;
     let pineapple = '';
+    console.log('child count:'+childCount)
 
-    for (let i=0; i<input; i++){
-        pineapple += '🍍';
+    if (wordArray.length === 2) {
+        if (wordArray[0] === 'clear'){
+            parsedInputOne = parseInt(wordArray[1]);
+            output.removeChild(output.children[parsedInputOne-1]);
+        }
+        else {
+            parsedInputOne = parseInt(wordArray[1]);
+            parsedInputZero = parseInt(wordArray[0]);
+            for (let i=0; i<parsedInputZero; i++){
+                pineapple += '🍍';
+            }
+            newPar.textContent = pineapple;
+            for (let j=0; j<parsedInputOne; j++){
+                output.appendChild(newPar);
+            }
+        }
+    } else if (wordArray.length === 1) {
+        if (wordArray[0] === 'clear') {
+            for(let i=0; i<childCount; i++){
+            output.removeChild(output.children[0]);
+            }
+        } else {
+            parsedInputZero = parseInt(wordArray[0]);
+            let pineapple = '';
+            for (let i=0; i<parsedInputZero; i++){
+                pineapple += '🍍';
+            }
+        }
+        newPar.textContent = pineapple;
+        output.appendChild(newPar);
     }
-    newPar.textContent = pineapple;
-    output.appendChild(newPar);
 };
