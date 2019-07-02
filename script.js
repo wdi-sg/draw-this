60826d6862736a81d42b6616318f6f513cff8b13
Nicholas_Bingei <nbinged@gmail.com>
2019-07-02 17:04:03 +0800

    First Commit

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 console.log("hello script js");
 
+//Input runs here
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+	drawPineapple(currentInput);
+	clearInput(currentInput);
+	clearInput2(currentInput);
 };
 
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+  // your DOM manipulation code here\
+	var output = document.querySelector('#output');
+        output.innerText = data;
+};	
+
+var drawPineapple = function(currentInput){
+	let pineapple = document.createElement("p");
+
+	for (let i = 0; i < currentInput; i++){
+		console.log(currentInput);
+		pineapple.textContent += "🍍";
+	};
+	var printPineapple = output.appendChild(pineapple);
+};
+
+var clearInput = function(currentInput){
+	if (currentInput === "clear"){
+
+	location.reload();
+	}
+};
+
+var clearInput2 = function(currentInput){
+	
+	if (currentInput){
+
+		Hello
+
+	}
+
+	
+
+	var listPineapples = output.childNodes;
+	listPineapples.removeChild(listPineapples.childNodes[0]);
 };
