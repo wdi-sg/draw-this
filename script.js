ab116927db3f540ae0042e66ebf941eaf2f52531
Stuart Myers <stuart.myers@gmail.com>
2019-12-16 17:33:14 +0800

    First version complete

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 console.log("hello script js");
 
+var outputDisplay = document.querySelector('#output');
+
+var emojiUsed = "🍍";
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  document.querySelector('#input').value = ""
+  currentInput = currentInput.trim().toLowerCase();
+  currentInput = parseInt(currentInput);
+  if (currentInput) {
+    display(emojiRow(currentInput));
+  }
 };
+
+var emojiRow = function(inputInteger) {
+  return emojiUsed.repeat(inputInteger);
+}
+
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+  var rowOutPut = document.createElement('p');
+  rowOutPut.innerText = stuffToDisplay;
+  outputDisplay.appendChild(rowOutPut);
 };
+
