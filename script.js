0bd4af1c51c6dcab2105e490e6889db7ecdf469a
benjacoblee <benjacoblee@gmail.com>
2019-12-16 17:56:07 +0800

    can create number of pineapples by rows

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 console.log("hello script js");
 let pineappleText = "";
+let pineappleParagraph;
 let input = document.querySelector("#input");
 let output = document.querySelector("#output");
 
 var inputHappened = function(currentInput) {
+  let numOfPineapples = currentInput;
   let sentence = currentInput;
   let wordArray = sentence.split(" ");
-  let pineapples = document.querySelectorAll(".pineapple");
-  let index = parseInt(wordArray[1]);
+  let pineapples = parseInt(wordArray[0]);
+  let row = parseInt(wordArray[1]);
 
-  console.log(parseInt(wordArray[0]));
-
-  if (currentInput === "clear") {
-    output.textContent = "";
-  } else if (wordArray.length > 1) {
-    if (isNaN(parseInt(wordArray[0]))) {
-      output.removeChild(output.childNodes[index - 1]);
-    } else {
-      // parseInt word array at position 0, do a loop for as many times as i < arry[0],
-      // every time creating a p element and appending it.
+  if (!isNaN(parseInt(numOfPineapples)) && wordArray.length === 1) {
+    for (let i = 0; i < numOfPineapples; i++) {
+      pineappleParagraph = document.createElement("p");
+      pineappleText = pineappleText + "🍍";
+      pineappleParagraph.textContent = pineappleText;
     }
-  }
-  display(currentInput);
+    output.appendChild(pineappleParagraph);
+    pineappleText = "";
+    input.value = "";
+  } else if (wordArray.length > 1) {
+    if (isNaN(pineapples)) {
+      output.removeChild(output.childNodes[row - 1]);
+      input.value = "";
+    } else {
+      for (i = 0; i < pineapples; i++) {
+        pineappleText = pineappleText + "🍍";
+      }
+      for (j = 0; j < row; j++) {
+        pineappleParagraph = document.createElement("p");
+        pineappleParagraph.textContent = pineappleText;
+        output.appendChild(pineappleParagraph);
+      }
+      pineappleText = "";
+      input.value = "";
+    }
+  } else if (currentInput === "clear") {
+    output.textContent = "";
+    input.value = "";
+  } 
 };
 
-var display = function(stuffToDisplay) {
-  // your DOM manipulation code here
-  for (let i = 0; i < stuffToDisplay; i++) {
-    pineappleText = pineappleText + "🍍";
-  }
-  let paragraph = document.createElement("p");
-  paragraph.classList.add("pineapple");
-  paragraph.textContent = pineappleText;
-  output.appendChild(paragraph);
-  pineappleText = "";
-  input.value = "";
-};
+var display = function(stuffToDisplay) {};
