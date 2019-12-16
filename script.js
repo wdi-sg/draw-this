917ed7c47fe05c9ea77d64ff33c37c697fa084ff
eunicelok <eunicelp511@gmail.com>
2019-12-16 23:55:22 +0800

    done version 1 but did not complete clear part

--- script.js
+++ script.js
@@ -13,17 +13,19 @@ var inputHappened = function(currentInput){
   var pineapple = ""; //declaring empty string to a variable, smth that u wana output later
   var displayPineapple = document.createElement('p'); //creating a p tag and store under variable named displayPineapple, creating p tag to show the pineapples but ntg inside it first, it's just being created but floating around
   for (var i = 0; i < parseInt(currentInput); i++) {
-        pineapple = pineapple + "🍍" // looping through current input and at the same time parseInt(currentInput) to turn any input into number, then pineapple starts increment by 1 and being stored in the pineapple variable
+        pineapple = pineapple + "🍍" // looping through current input and at the same time parseInt(currentInput) to turn any input into number, then pineapple starts increment by 1 and being stored in the pineapple variable. this one no .length coz is parse (numbers)
 
   }
   displayPineapple.innerText = pineapple // the pineapple will appear inside the p tag being created
-  console.log(pineapple);
-  console.log(displayPineapple);
-document.getElementById('output').appendChild(displayPineapple) // adding pineapple emojis into the output box, document.getElementById('output') is in html there
+  // console.log(pineapple);
+  // console.log(displayPineapple);
+document.getElementById('output').appendChild(displayPineapple);
+// function clearText() {
+//     if (currentInput === 'clear'); {
+//         var myobj = document.getElementByClassName("starter");
+//     myobj.remove();
+//     }
+// }; to work on clear part
 
-// if (currentInput === 'clear') {
-//     var clearText = document.getElementById('output').removeChild(displayPineapple);
-//     return clearText;
-// }
 
-};
+};// adding pineapple emojis into the output box, document.getElementById('output') is in html there
