f3fb83d77c6be3276857c60bcd20204a0dcd119c
upieez <samuelhww@gmail.com>
2019-12-17 00:47:01 +0800

    add function for triangle

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 console.log("emoji assignment");
 
 //global variables
+document.getElementById("output").innerHTML = "Enter desired emoji!"
 var stuff = document.getElementById("output");
 var totalEmoji = "";
 var emoji = ""
+var emojiTri = "";
 var enteredEmoji = false;
+
 
 
 //splits the input into an array
@@ -12,38 +15,53 @@ var splitWord = function(input){
     var words = input
     var array = words.split(" ");
     return array;
-}
+};
 
-// clears the output box
-var clear = function(input){ //
+// clears the output box and text box
+var clear = function(){ //
     stuff.innerHTML = " ";
-}
+    document.getElementById("input").value = "";
+};
+
+// create normal triangle function
+var triangle = function(){
+        var p = document.createElement("p");
+        emojiTri += emoji;
+        p.innerHTML = emojiTri;
+        stuff.appendChild(p);
+};
 
 // input box
 var inputHappened = function(currentInput){
     if (enteredEmoji === false){
         emoji = currentInput;
+        clear();
         enteredEmoji = true;
     }
     var array = splitWord(currentInput);
     var arrayNum = parseInt(array[1]);
     console.log(typeof arrayNum)
     if (array.length === 1 && array[0] === "clear"){
-        clear(currentInput);
+        clear();
     } else if (array[0] === "clear" && array.length > 1 ){
         stuff.removeChild(stuff.childNodes[arrayNum - 1]);
-    } else if (!isNaN(array[1])){
-        for(var i = 0; i < arrayNum; i++){
+    } else if (!isNaN(array[0]) && !isNaN(array[1])){
+        for(var i = 0; i < arrayNum; i++){ // display the number of rows
             display(array[0]);
         };
-    } else {
+    } else if (array[0] === "triangle"){
+        for(i = 0; i < arrayNum; i++){
+            triangle();
+        }
+    }else {
         display(currentInput);
     }
 };
+
 // output box
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  var p = document.createElement("p")
+  var p = document.createElement("p");
   for (var i = 0; i < stuffToDisplay; i++){
     totalEmoji = totalEmoji + emoji;
   }
