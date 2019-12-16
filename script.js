e0d7bbc62aa16271b11227f52b3f40f530c5681d
Jess <jessicatanhc@gmail.com>
2019-12-16 19:27:56 +0800

    Clear row entered

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display(currentInput);
-};
-
+var deleteRow = function(){
+    var output = document.querySelectorAll("#output p");
+     output[num-1].remove("#output p");
+}
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
     var pTag = document.createElement('p');
     var emoji = "";
+
     for(var i = 0; i< stuffToDisplay;i++){
         emoji += "😃";
     }
+    pTag.className = "emoji";
     pTag.innerText = emoji;
    var output = document.querySelector("#output");
     output.appendChild(pTag);
    return emoji;
 };
+var inputHappened = function(currentInput){
+  console.log( currentInput);
+  //if it can be split
+  var wordArray = currentInput.split(" ");
+  var num = parseInt(wordArray[1]);
+  if(wordArray[0] === 'clear' && !isNaN(num)){
+    deleteRow();
+  }
+  currentInput = parseInt(currentInput);
+  if(!isNaN(currentInput)){
+  display(currentInput);
+    }
+}
