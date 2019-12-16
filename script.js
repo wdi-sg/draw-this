6172cfc85bd3db7a5c70cbf0cd8792e4934a6ce6
upieez <samuelhww@gmail.com>
2019-12-16 20:41:29 +0800

    add function to clear, split input and displaying emoji

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 console.log("hello script js");
+var stuff = document.getElementById("output");
+
+var totalEmoji = "";
+var emoji = "🍣"
+
+var splitWord = function(input){
+    var words = input
+    var array = words.split(" ");
+    return array;
+}
+
+var clear = function(input){
+    stuff.innerHTML = " ";
+}
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    var array = splitWord(currentInput);
+    var arrayNum = parseInt(array[1]);
+    console.log(typeof arrayNum)
+    if (array.length === 1 && array[0] === "clear"){
+        console.log("clear runs")
+        clear(currentInput);
+    } else if (array.length > 1){
+        stuff.removeChild(stuff.childNodes[arrayNum - 1]);
+    } else {
+        display(currentInput);
+    }
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+  var p = document.createElement("p")
+  for (var i = 0; i < stuffToDisplay; i++){
+    totalEmoji = totalEmoji + emoji;
+  }
+  p.innerHTML = totalEmoji;
+  totalEmoji = "";
+  stuff.appendChild(p)
 };
