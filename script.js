5c031bbf107cb9376317d4a679e9935069b4f3d6
Jess <jessicatanhc@gmail.com>
2019-12-16 17:50:45 +0800

    Take in number and draw figures

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display(currentInput);
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+    var pTag = document.createElement('p');
+    var emoji = "";
+    for(var i = 0; i< stuffToDisplay;i++){
+        emoji += "😃";
+    }
+    pTag.innerText = emoji;
+   var output = document.querySelector("#output");
+    output.appendChild(pTag);
+   return emoji;
 };
