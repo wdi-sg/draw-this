d88dcd0cb9f089d2fbf59a0eec50acb8b26549e9
upieez <samuelhww@gmail.com>
2019-12-16 22:18:15 +0800

    add loop for rows with two input

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-console.log("hello script js");
-var stuff = document.getElementById("output");
+console.log("emoji assignment");
 
+//global variables
+var stuff = document.getElementById("output");
 var totalEmoji = "";
 var emoji = "🍣"
 
+
+//splits the input into an array
 var splitWord = function(input){
     var words = input
     var array = words.split(" ");
     return array;
 }
 
-var clear = function(input){
+// clears the output box
+var clear = function(input){ //
     stuff.innerHTML = " ";
 }
 
+// input box
 var inputHappened = function(currentInput){
     var array = splitWord(currentInput);
     var arrayNum = parseInt(array[1]);
     console.log(typeof arrayNum)
     if (array.length === 1 && array[0] === "clear"){
-        console.log("clear runs")
         clear(currentInput);
-    } else if (array.length > 1){
+    } else if (array[0] === "clear" && array.length > 1 ){
         stuff.removeChild(stuff.childNodes[arrayNum - 1]);
+    } else if (!isNaN(array[1])){
+        for(var i = 0; i < arrayNum; i++){
+            display(array[0]);
+        };
     } else {
         display(currentInput);
     }
 };
-
+// output box
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
   var p = document.createElement("p")
