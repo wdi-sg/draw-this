b96b1acbb986cdeb235ac8383ca797c0257f85e5
joycemap <57893220+joycemap@users.noreply.github.com>
2019-12-16 19:01:56 +0800

    add version 1

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  console.log(currentInput );
+  display( currentInput );
 };
 
-var display = function(stuffToDisplay){
+var display = function(stufftoDisplay){
   // your DOM manipulation code here
+  console.log(stufftoDisplay)
+  var pinkbox = document.getElementById("output") 
+  var text = ""
+    for (var i = 0; i < parseInt(stufftoDisplay);i++){
+     text = text + "🍍";
+    }
+    var emoji = document.createElement('p')
+    emoji.innerHTML = text
+    pinkbox.appendChild(emoji)
   
-};
+  
+
+  }
+ // ;
+
+//};
