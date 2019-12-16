2bb92c03f769f5857d08be83f2da60c1a09691a7
SamTanFL <samsite_tan@hotmail.com>
2019-12-16 19:01:22 +0800

    part 1 done

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 console.log("hello script js");
 
+var pineapple = "🍍"
+var pineColumns = "";
+var userInput;
+
+
+var clearInput = function() {
+    document.querySelector("#input").value = "";
+}
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    userInput = parseInt(currentInput);
+    if (userInput > 0){
+        makePara();
+        makePineaples(userInput);
+        insertPineapples();
+        clearInput();
+        pineColumns = "";
+    } else if (currentInput.toLowerCase() === "clear") {
+        clearPineapples();
+        clearInput();
+    }
 };
 
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+    // your DOM manipulation code here
+    document.querySelector("#output").innerText = stuffToDisplay;
+
 };
+
+var makePara = function() {
+    var pinePara = document.createElement("p");
+    pinePara.setAttribute("class", "pineapple")
+    document.querySelector("#output").appendChild(pinePara);
+
+}
+
+var makePineaples = function (numPine) {
+    for ( i = 0 ; i < numPine ; i++ ){
+        pineColumns = pineColumns + pineapple;
+    }
+    return pineColumns;
+}
+
+var insertPineapples = function () {
+    document.querySelector("p:last-child").innerText = pineColumns;
+}
+
+var clearPineapples = function () {
+    var output = document.querySelector("#output");
+    output.innerHTML = "";
+}
