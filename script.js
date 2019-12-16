dffeafc455b7f5a3ec4c7d9701b9e701db5341ae
Robert Kolsek <kolsek.income@gmail.com>
2019-12-16 15:18:21 +0800

    first and second version done

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-console.log("hello script js");
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  //take multiple inputs, split into an array
+  let inputArr = currentInput.split(" ")
+//check if command is to clear
+  if (inputArr[0] === "clear"){
+    return inputArr
+  }
+
+  let pineapple = "🍍"
+  let pineappleContainer = []
+  let pineappleParagraph = document.createElement("p")
+  // iterate input array and push same amount of pineapples as input
+  for (let i = 0; i < inputArr[0]; i++){
+    pineappleContainer.push(pineapple)
+  }
+  // set paragraph to amount of pineapples as a string
+  pineappleParagraph.innerHTML = `${pineappleContainer.join("")}`
+  // return the paragraph
+  return pineappleParagraph
 };
 
 var display = function(stuffToDisplay){
+  // take pineapple paragraph as parameter
+
   // your DOM manipulation code here
-  
+  let displayElement = document.querySelector("#output")
+  // check if not clear, append
+  if (stuffToDisplay[0] !== "clear") {
+    displayElement.appendChild(stuffToDisplay)
+  }
+  // if input is clear, remove node as in second number
+  if (stuffToDisplay[0] === "clear") {
+    displayElement.removeChild(displayElement.childNodes[stuffToDisplay[1]])
+  }
+  document.querySelector("#input").value = ""
 };
