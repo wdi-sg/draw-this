e3e1861d40c1293beecf5b12a4d178674db4c594
joyce-paul <joyce_paul@hotmail.com>
2019-12-16 19:21:20 +0800

    Created the first step.

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  console.log(currentInput);
+  display(currentInput);
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+console.log(stuffToDisplay);
+var box = document.getElementById("output");  // This refers to the output box
+
+
+var text = "";
+
+for (var i = 0; i < parseInt(stuffToDisplay); i++) {
+    text = text + "🍍";
+}
+
+var emoji = document.createElement("p");
+    emoji.innerHTML = text;
+    box.appendChild(emoji);
+
+//append to the output element
 };
+
+
+/*
+Create a program that takes in commands to draw different figures.
+
+First Version
+Your program will take a command to draw a "row", (it will be a <p> tag) with the number of columns you want to draw.
+
+
+Start by outputting a 🍍for each column.
+
+Example:
+
+You input: 3.
+
+Your program draws:
+
+🍍🍍🍍
+Your HTML would look like this:
+
+<p>🍍🍍🍍</p>
+
+
+
+You input 2.
+
+Your program draws a new row, adding to what's already there:
+
+🍍🍍🍍
+🍍🍍
+You can type clear to clear the elements out of the DOM.*/
