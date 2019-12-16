b2f3a271d3dd111672fbcc271804b57e13db38f1
hweechieh <hweechieh14@gmail.com>
2019-12-17 05:35:09 +0800

    Part 1 Completed

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+  if (isNaN(currentInput === true || currentInput === '0' )) {
+    return "Invalid input!"
+  };
+  display(currentInput);
+
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+
+    var emojiUsed = "🌼";
+    //creating a box to keep my <p> tag
+    var box = document.getElementById("output");
+    //<p> tag
+    var pTag = document.createElement("p");
+
+    for (let i = 0; i < stuffToDisplay; i++) {
+        pTag.innerText += emojiUsed;
+    };
+    box.appendChild(pTag);
 };
