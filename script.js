09a697b0e68f58959d610c960de38486a3d7bfdf
ShaneC <shanes_email_is@yahoo.com>
2019-12-17 02:23:34 +0800

    added in emoji.

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ var inputHappened = function(currentInput){
   clearInput(currentInput);
   clearInput2(currentInput);
 };
+
+var display = function(stuffToDisplay){
+  // your DOM manipulation code here
+  var output = document.querySelector('#output');
+  output.innerText = data;
+
+};
+
+var drawVegetable = function(currentInput) {
+    let vegetable = document.createElement("p");
+
+    for (let i = 0; i < currentInput; i++) {
+        console.log(currentInput);
+        vegetable.textContent += "🥬";
+    };
+}
