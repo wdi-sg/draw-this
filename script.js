1ff5f56883157bebeeaa22c2f553ecbccc4021da
joyce-paul <joyce_paul@hotmail.com>
2019-12-17 07:57:48 +0800

    tried to work on clear function. did not work here.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var inputHappened = function(currentInput){
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
 console.log(stuffToDisplay);
-var box = document.getElementById("output");  // This refers to the output box
 
+var box = document.getElementById("output");  // This refers to the output box
 
 var text = "";
 
@@ -19,38 +19,21 @@ for (var i = 0; i < parseInt(stuffToDisplay); i++) {
 
 var emoji = document.createElement("p");
     emoji.innerHTML = text;
-    box.appendChild(emoji);
+    box.appendChild(emoji); //append to the output element
 
-//append to the output element
+
+
 };
 
 
-/*
-Create a program that takes in commands to draw different figures.
 
-First Version
-Your program will take a command to draw a "row", (it will be a <p> tag) with the number of columns you want to draw.
+/*    box.removeChild(emoji);*/
 
 
-Start by outputting a 🍍for each column.
+/*function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+*/
 
-Example:
-
-You input: 3.
-
-Your program draws:
-
-🍍🍍🍍
-Your HTML would look like this:
-
-<p>🍍🍍🍍</p>
-
-
-
-You input 2.
-
-Your program draws a new row, adding to what's already there:
-
-🍍🍍🍍
-🍍🍍
-You can type clear to clear the elements out of the DOM.*/
