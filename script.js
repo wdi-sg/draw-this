49942e40e9ed2daf88ce6f6857504e3869cd9662
rachellesg <rachelle.yeo@outlook.com>
2019-12-16 23:22:58 +0800

    added in part 2 clear, plus multi rows

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ console.log("hello script js");
 var icon = "🍀";
 var output = document.querySelector('#output');
 
-var totalClovers = "";
+var totalClovers = " ";
 var addedClovers = 0;
 
-// totalClovers = totalClovers + (input clover)
+var firstRowIcons = " ";
+var secondRowIcons = " ";
+
+var clovers = 0;
+
+var count = 0;
+
+var clearInput = function () {
+    document.querySelector("#input").value = "";
+}
 
 var inputHappened = function(currentInput){
     display(currentInput);
@@ -16,21 +25,32 @@ var inputHappened = function(currentInput){
     }
 };
 
-var clearInput = function () {
-    document.querySelector("#input").value = "";
-}
-
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
     var cloverRows = document.createElement("p");
-    for (var i = 0; i < stuffToDisplay; i++) {
-        totalClovers += icon;
-    }
+    var secondRow = document.createElement("p");
+    if (stuffToDisplay.includes(" ")) {
+        var displaySplit = stuffToDisplay.split(" ");
+        var firstRow = displaySplit[0];
+        var secondRow = displaySplit[1];
+        for (var i = 0; i < firstRow; i++) {
+            firstRowIcons += icon;
+        }
+        for (var i = 0; i < secondRow; i++) {
+            secondRowIcons += icon;
+        }
+        cloverRows.innerHTML = "<p>" + (firstRowIcons) + "</p>" + "<p>" + (secondRowIcons) + "</p>";
+        secondRow.innerText = (secondRowIcons);
+        console.log(firstRow, secondRow);
+        document.querySelector("#output").appendChild(cloverRows);
+        firstRowIcons = " ";
+        secondRowIcons = " ";
+    } else if (isNaN(stuffToDisplay) === false) {
+        for (var i = 0; i < stuffToDisplay; i++) {
+            totalClovers += icon;
+        }
         cloverRows.innerText = (totalClovers);
         document.querySelector("#output").appendChild(cloverRows);
         totalClovers = "";
-};
-
-var appendIX = function (cloverRows) {
-    // append p tags and stuff to output
+    }
 };
