d2d6e9c6b47947b2d1852f29a5dec7b4f9c561bb
SafrazHakamali <safrazhakamali@gmail.com>
2019-12-16 22:48:05 +0800

    completed second version

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 console.log("hello script js");
-var a=""
+var a="";
+var num = 0;
 var outputField=document.querySelector('#output');
 var inputField=document.querySelector('#input');
+var count = 0;
+
 var inputHappened = function(currentInput){
-	var b = document.createElement('p');
-	for (i = 0; i < currentInput; i++) {
-		a = a + "🍍";
+	if (currentInput.includes(" ")) {
+		num = parseInt(currentInput.split(" ")[1]);
+		document.querySelector('#p-'+num).innerHTML = "";
+	} else {
+		var b = document.createElement('p');
+		count++
+		for (i = 0; i < currentInput; i++) {
+			a = a + "🍍";
+		}
+		b.innerHTML=a;
+		b.id="p-"+ count;
+		outputField.appendChild(b);
+		inputField.value = "";
+		a="";
 	}
-	b.innerHTML=a;
-	outputField.appendChild(b);
-	inputField.value = "";
-	a="";
-};
+}
 
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
 };
