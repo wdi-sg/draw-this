7b3494b66b52769348accf70a047526259391af3
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2019-12-17 08:06:30 +0800

    Unfinishied code -cannot loop display of icon

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+//store number of columns input by user
+var iconColumn = 0;
 
+//store icon to print
+var drawing = "🍍";
+
+//to append new output to previous output
+var iconDisplay = document.querySelector("#output");
+
+
+var inputHappened = function(currentInput){
+    console.log(iconColumn);
+    iconColumn = parseInt(currentInput);
+    display(iconColumn);
+};
+debugger;
+
+//DOM manipulation code here.
+//Display output each time in a new row
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+    // repeat icons to display
+    for (var i = 0; i < iconColumn; i++) {
+    iconDisplay.textContent = drawing;
+    console.log(iconDisplay);
+    }
+    var outputIcons = document.createElement("p");
+    outputIcons.innerHTML = stuffToDisplay;
+    iconDisplay.appendChild(outputIcons);
 };
