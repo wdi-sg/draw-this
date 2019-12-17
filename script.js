3b5a9c55e9e55b863170a81add19b4dce5f93922
joycemap <57893220+joycemap@users.noreply.github.com>
2019-12-17 09:03:34 +0800

    included clear function

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ var inputHappened = function(currentInput){
   display( currentInput );
 };
 
-var display = function(stufftoDisplay){
+var display = function(stufftoDisplay){//changing the binding of the variable from stufftoDisplay to currentInput
   // your DOM manipulation code here
   console.log(stufftoDisplay)
-  var pinkbox = document.getElementById("output") 
+  var pinkbox = document.getElementById("output") //To tie the pinkbox to the output Id
   var text = ""
-    for (var i = 0; i < parseInt(stufftoDisplay);i++){
-     text = text + "🍍";
+    for (var i = 0; i < parseInt(stufftoDisplay);i++){//for loop to tie current Input to the output
+     text = text + "🍍";//to concatenate the "pineapples"
     }
-    var emoji = document.createElement('p')
+    var emoji = document.createElement('p')//create p tag
     emoji.innerHTML = text
-    pinkbox.appendChild(emoji)
+    pinkbox.appendChild(emoji)//append pineapples to output box
+  
+    if (stufftoDisplay === "clear"){
+     document.getElementById("output").InnerHTML = " "}
+    }
+    
+    
   
   
-
-  }
  // ;
 
 //};
+//
