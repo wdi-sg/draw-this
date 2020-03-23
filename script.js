f6498b2c9f6748d04ea9da28a0b6ecf603e76622
Rachelik <rachel.ik.87@gmail.com>
2020-03-23 22:25:30 +0800

    Add first row

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 console.log("hello script js");
+var threePineapple = "🍍🍍🍍";
+var twoPineapple = "🍍🍍";
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  if (parseInt(currentInput) === 3) {
+    display("🍍🍍🍍");
+  } else if (parseInt(currentInput) === 2) {
+    return display(pineappleTwoRow);
+  }
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+
+// var display = function(stuffToDisplay){
+//   // your DOM manipulation code here
+//   var output = document.querySelector('#output')
+//     output.innerHTML = stuffToDisplay;
+//     var document.querySelector()
+// };
