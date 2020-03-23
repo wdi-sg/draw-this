ade1ff0c90f6c38444085ed98b9d9cfa969f17f6
kwansing14 <kwansing@hotmail.sg>
2020-03-24 00:40:07 +0800

    input and clear done

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 console.log("hello script js");
-
+var pineapple = "" ;
+var total=0;
 var inputHappened = function(currentInput){
   console.log( currentInput );
+  data = currentInput;
+  pineapple = "";
   display( "WOW SOMETHING HAPPENED" );
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+
+  //createpineapple
+  var pcreate = document.createElement("p");
+  if (isNaN(parseInt(data))==false) {
+    console.log("pineapple")
+    for (var i=0; i<data; i++) {
+        pineapple += "🍍";
+    }
+    total++;
+    pcreate.innerText = pineapple;
+  }
+
+  //clear
+  if (data == "clear") {
+    console.log("clear")
+    console.log(total);
+    for (var i=0; i<total; i++) {
+        var input = document.getElementById("output");
+        var pquery = input.querySelector("p");
+        input.removeChild(pquery);
+    }
+    total=0;
+  }
+    var input = document.getElementById("output");
+    input.appendChild(pcreate);
 };
