ead11435abe984378595073cbdefdbf4157ac97c
mdazharaw <darkaz62@hotmail.com>
2020-03-23 21:48:19 +0800

    Part 1 done.

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display(currentInput);
 };
 
 var display = function(stuffToDisplay){
+  var content ="";
+  var repeat = parseInt(stuffToDisplay);
+  for (i=0;i<repeat;i++){
+    content += "🍍";
+    }
+var output = document.getElementById("output");
+output.innerHTML = content;
   // your DOM manipulation code here
-  
+
 };
