c28cc1922f9559b96da080d29f72514225604668
chelsejw <chelsejw@gmail.com>
2020-03-24 06:31:04 +0800

    forgot clear function for v1.0

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 console.log("hello script js");
 
 function clearInput() {
-  document.getElementById("input").value = "";
+    document.getElementById("input").value = "";
 }
 
-var inputHappened = function(currentInput){
-  clearInput();
-  var newArray = [];
-  for (var i=0; i < currentInput; i++) {
-    newArray.push("🍍");
-  };
-  var newRow = document.createElement("p");
-  newRow.textContent = newArray.join("");
-  return display(newRow);
-};
+var inputHappened = function(currentInput) {
+    clearInput();
 
-var display = function(stuffToDisplay){
-  document.getElementById("output").appendChild(stuffToDisplay)
+    if (currentInput === "clear") {
+        return document.getElementById("output").textContent = "";
+    } else {
+        var newArray = [];
+        for (var i = 0; i < currentInput; i++) {
+            newArray.push("🍍");
+        };
+        var newRow = document.createElement("p");
+        newRow.textContent = newArray.join("");
+        return display(newRow);
+    };
+}
+
+var display = function(stuffToDisplay) {
+    document.getElementById("output").appendChild(stuffToDisplay)
 };
