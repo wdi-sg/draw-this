714c8f85f7edd727b1fe8e2e61e614774616f450
kwansing14 <kwansing@hotmail.sg>
2020-03-24 01:22:33 +0800

    done with multi row input

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 console.log("hello script js");
 var pineapple = "" ;
 var total=0;
+var split = [];
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
   data = currentInput;
   pineapple = "";
   display( "WOW SOMETHING HAPPENED" );
+  document.getElementById("input").value = "";
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
 
+  //splitter
+  if (data.includes(" ") == true) {
+    console.log("empty space")
+    split = data.split(" ");
+  }
+
   //createpineapple
-  var pcreate = document.createElement("p");
-  if (isNaN(parseInt(data))==false) {
+  if (isNaN(parseInt(data))==false & data.includes(" ")==false) {
+    var pcreate = document.createElement("p");
     console.log("pineapple")
     for (var i=0; i<data; i++) {
         pineapple += "🍍";
     }
     total++;
     pcreate.innerText = pineapple;
+    var input = document.getElementById("output");
+    input.appendChild(pcreate);
   }
 
   //clear
@@ -33,6 +44,32 @@ var display = function(stuffToDisplay){
     }
     total=0;
   }
-    var input = document.getElementById("output");
-    input.appendChild(pcreate);
+
+  //clear selected rows
+  if (split[0] == "clear") {
+    console.log("splitclear");
+    console.log("split[1]");
+    var out = document.getElementById("output");
+    var out2 = out.children[split[1]-1];
+    out.removeChild(out2);
+  }
+
+  //insert multiple row
+  if (isNaN(parseInt(split[0]))==false) {
+    console.log("multi-input")
+    console.log("split")
+    for (var i=0; i<split[0]; i++) {
+        var pcreate = document.createElement("p");
+        for (var j=0; j<split[1]; j++) {
+            pineapple += "🍍";
+        }
+        pcreate.innerText = pineapple;
+        pineapple = "";
+        var input = document.getElementById("output");
+        input.appendChild(pcreate);
+    }
+    total = total + parseInt(split[0]);
+    split = [];
+  }
+
 };
