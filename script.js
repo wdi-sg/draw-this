3bfc0d8d6dd4dd9b65ada0e1e880bcfe2ae4b160
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-23 23:13:22 +0800

    First commit

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 console.log("hello script js");
 
+let output = "";
+
+const init = function(){
+    document.getElementById('input').value = "";
+}
+
+
 var inputHappened = function(currentInput){
+  if(currentInput === "clear"){
+    clear();
+  }
+
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display( currentInput );
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+  const body = document.querySelector('body');
+  const input = document.createElement('p');
+  input.className = "inputClass";
+
+  for(let i = 0; i<stuffToDisplay; i++){
+    output = output + "🍍"
+  }
+  input.textContent = output ;
+  body.appendChild(input);
+  output = "";
+  init();
 };
+
+var clear = function(){
+    const para = document.querySelectorAll('.inputClass');
+    for(let p = 0; p<para.length; p++){
+        para[p].textContent = "";
+    }
+}
