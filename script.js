394dd2a96b8d3439a4c84a0d09be4eb214d92e5e
jordanswp <sohweepin@gmail.com>
2020-03-24 01:35:41 +0800

    first edits to homework

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+var pineappleVar = "";
+var userInput = document.querySelector("#input");
+var pineapple = document.createElement("p");
+for (var i = 0; i < parseInt(currentInput); i++){
+    pineappleVar = pineappleVar + "🍍"
+}
+
+pineapple.textContent = pineappleVar;
+document.getElementById('div').appendChild(pineapple);
+    userInput.value = "";
+}
