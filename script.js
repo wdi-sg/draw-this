947e0212af0980d6f0a464c19d4afd66439cba02
vigbit <vigneshb@live.com>
2020-03-24 07:23:24 +0800

    First versrion

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display( currentInput);
 };
+
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+
+  var list = document.getElementById("output")
+  var myNewList = document.createElement('p');
+
+  var i = parseInt(stuffToDisplay);
+
+  myNewList.textContent = "🍍".repeat(i);
+  list.appendChild(myNewList);
+
 };
