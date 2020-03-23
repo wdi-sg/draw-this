989c6b506270a5bd3729c48455a8016c5d8a9b1d
kenneththesheep <kenneththesheep@gmail.com>
2020-03-23 23:36:23 +0800

    can insert lines can clear. Weird logic though. At first clear even lines, then clear everything except last line. Managed to overcome

--- script.js
+++ script.js
@@ -1,11 +1,79 @@
-console.log("hello script js");
+//Global Variables
+var pineappleCalled;
+var callType;
+var inputArray;
+var numberOfCurrentRow=0;
 
+//console.log("hello script js");
+ //🍍 onglai
+// for checking input
+
+//To insert Row
+InsertRow=(pineappleInsert)=>{
+    var newRowItem=[];
+    var newRowID, textDisplayed;
+    var newParagraph=document.createElement('p');
+    var outputDisplay=document.getElementById("output");
+    numberOfCurrentRow++;
+    //class name cannot contain space
+    newRowID=numberOfCurrentRow.toString();
+    for(var i=0; i<pineappleInsert;i++)
+    {
+        if(i===0)
+        {
+            newRowItem=["🍍"];
+
+        }
+        else
+        {
+            newRowItem.push("🍍")
+        }
+    }
+    textDisplayed=newRowItem.join('');
+    console.log(textDisplayed);
+    newParagraph.classList.add('row');
+    newParagraph.setAttribute("id",newRowID);
+    newParagraph.innerText=textDisplayed;
+    outputDisplay.appendChild(newParagraph);
+
+}
+
+//clear all
+var clear=()=>{
+    var rowRemoval=document.getElementsByClassName("row");
+    var itemRemoval;
+    var output=document.getElementById("output");
+        for(var i=0;i<=rowRemoval.length;i++)
+        {
+                //itemRemoval=document.getElementsByClassName("row")[i];
+            //output.removeChild(itemRemoval);
+            output.removeChild(rowRemoval[0]);
+        }
+output.removeChild(rowRemoval[0]);
+}
+
+//Where the magic happens
 var inputHappened = function(currentInput){
-  console.log( currentInput );
+    inputArray=currentInput.split(' ');
+  //console.log( inputArray );
+ //to note if double digit,, double array
+  if(inputArray.length===1)
+  {
+    if(isNaN(parseInt(inputArray[0]))){
+        if(inputArray[0].toLowerCase()==="clear")
+        {
+            clear();
+        }
+    }
+    else
+    {pineappleCalled=parseInt(inputArray[0]);
+    InsertRow(pineappleCalled);}
+  }
   display( "WOW SOMETHING HAPPENED" );
+  inputArray=[];
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+
 };
