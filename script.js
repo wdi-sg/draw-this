c88828fed20356a6dba8f4750d90b02bd40450af
Rachelik <rachel.ik.87@gmail.com>
2020-03-24 00:05:54 +0800

    Part 3 done

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var resetOutput = function() {
 
 var makePineappleArray = function() {
     var pineappleText = output.innerHTML;
-    pineappleArray = pineappleText.split('<br>');
+    pineappleArray = pineappleText.split('</p><p>');
     pineappleArray.splice(1,1);
     output.innerHTML = pineappleArray[0];
 }
@@ -23,14 +23,14 @@ var makePineappleArray = function() {
 var inputHappened = function(currentInput){
   console.log(currentInput);
   resetInput();
-  if (parseInt(currentInput) === 3) {
-    display("🍍🍍🍍");
+  if (currentInput === "3") {
+    display("<p>🍍🍍🍍</p>");
   };
 
   resetInput();
 
-  if (parseInt(currentInput) === 2) {
-    display("<br>") + display("🍍🍍");
+  if (currentInput === "2") {
+    display("<p>🍍🍍</p>");
   };
 
   if (currentInput.toLowerCase() === 'clear') {
@@ -40,9 +40,14 @@ var inputHappened = function(currentInput){
   if (currentInput.toLowerCase() === 'clear 2') {
     makePineappleArray();
   }
+
+  if (currentInput === "2 2") {
+    display("<p>🍍🍍</p>") + display("<p>🍍🍍</p>");
+  }
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
     output.innerHTML += stuffToDisplay;
+
 };
