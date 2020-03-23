f00b5d856ac1b3743b0b55fb7ee50a3d4a500638
kokchuantan <kokchuan1996@gmail.com>
2020-03-23 21:12:56 +0800

    part 1

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 console.log("hello script js");
 
+var inputArray = [];
+var bird = '🐦';
+var input , x;
+var birdOutput = '';
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  var input = currentInput;
+  if(!isNaN(parseInt(input))){
+    inputArray.push(input);
+    console.log( currentInput );
+    //display( "WOW SOMETHING HAPPENED" );
+
+    for(x = 0 ; x < input ; x++){
+      birdOutput += bird;
+      console.log(birdOutput);
+    }
+    display();
+  }
+  else if (input === 'clear'){
+    document.querySelector('#output').innerText ='';
+  }
+  else{
+    document.querySelector('#output').innerText ='Invalid Character';
+  }
 };
 
-var display = function(stuffToDisplay){
+var display = function(){
   // your DOM manipulation code here
-  
+  var newPara = document.createElement('p');
+    newPara.innerText = birdOutput;
+    document.getElementById('output').appendChild(newPara);
+    birdOutput = '';
 };
