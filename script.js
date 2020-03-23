8befd98a493f479d974158d94ebb00e3ec313e4f
elisaes <alisa260785@gmail.com>
2020-03-24 00:09:59 +0800

    first part done

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+let currentInput;
+const myEmoji = [];
+
+var inputHappened = function(currentInput) {
+  document.getElementById('input').value = "";
+  for (let i = 0; i < currentInput; i++) {
+    myEmoji[i] = "🦠";
+    myEmoji.push();
+  }
+  stuffToDisplay = myEmoji;
+  console.log(currentInput !== 0);
+  if (currentInput !== 0) {
+    let newParagraph = document.createElement("p");
+    newParagraph.innerHTML = stuffToDisplay;
+    document.body.appendChild(newParagraph);
+  }
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+var display = function(stuffToDisplay) {
+// your DOM manipulation code here
+
 };
