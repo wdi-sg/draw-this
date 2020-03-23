174ebcb9a2b35ffdeadf85d66b658d1ae44ec5f1
mdazharaw <darkaz62@hotmail.com>
2020-03-23 23:45:28 +0800

    Part 1 - Second version done.

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 console.log("hello script js");
 
-// Part 1 - First version
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  if (currentInput == "clear"){
+// Part 1 - Second version
+var inputHappened = function(currentInput) {
+    console.log(currentInput);
     var output = document.getElementById("output");
-    output.innerHTML="";
-  }
-  display(currentInput);
+    var inputArray = currentInput.split(" ");
+    console.log(inputArray);
+    var int = parseInt(inputArray[1] - 1);
+    console.log(int);
+    if (currentInput == "clear") {
+        output.innerHTML = "";
+    } else if (inputArray[0] == "clear") {
+        var content = output.innerHTML;
+        console.log(content);
+        var contentArray = content.split("<br>");
+        console.log(contentArray);
+        contentArray.splice(int, 1);
+        console.log(contentArray);
+        var newContent = "";
+        for (i = 0; i < contentArray.length; i++) {
+            newContent += contentArray[i];
+            if (i != contentArray.length - 1) {
+                newContent += "<br>";
+            }
+            console.log(contentArray[i]);
+            console.log(newContent);
+
+        }
+
+        output.innerHTML = newContent;
+
+    } else if (!isNaN(currentInput) && currentInput != "") {
+        display(currentInput);
+    }
 };
 
-var display = function(stuffToDisplay){
-  var output = document.getElementById("output");
-  var content = "";
-  var repeat = parseInt(stuffToDisplay);
-  for (i=0;i<repeat;i++){
-    content += "🍍";
+var display = function(stuffToDisplay) {
+    var output = document.getElementById("output");
+    var content = "";
+    var repeat = parseInt(stuffToDisplay);
+    for (i = 0; i < repeat; i++) {
+        content += "🍍";
     }
-    content += "<br/>"
-output.innerHTML += content;
-  // your DOM manipulation code here
+    content += "<br>"
+    output.innerHTML += content;
 
 };
