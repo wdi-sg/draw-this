a45e61563259dd8965d3a99abb672197ccc642a6
osbornechan <osbornecab@gmail.com>
2020-03-23 22:40:28 +0800

    First commit

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 console.log("hello script js");
 
+var clearInput = function() {
+    return document.getElementById("input").value = '';
+}
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    clearInput();
+    display(currentInput);
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+var display = function(currentInput) {
+
+    var pArray = [];
+
+    if (!isNaN(parseInt(currentInput)) == true) {
+        for (var i = 0; i < currentInput; i++) {
+        pArray.push('🍍');
+        }
+    } else {
+        var inputArray = currentInput.split(' ');
+        var inputArrayIndex = inputArray[1];
+        var pClear = document.querySelectorAll('.pineapple');
+            pClear[inputArrayIndex - 1].innerHTML = "";
+    }
+
+    var myPText = pArray.join('');
+    var myP = document.createElement('p');
+    myP.classList.add('pineapple');
+    myP.innerText = myPText;
+
+    return document.body.appendChild(myP);
 };
