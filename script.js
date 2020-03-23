cd00ebf76abb2921b4d389b89d3afcbcb94799bf
elisaes <alisa260785@gmail.com>
2020-03-24 01:50:05 +0800

    did first part

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
+const textInputHandler = (event) => {
+  const currentInput = event.target.value;
+  inputHappened(currentInput);
+}
+
+document.querySelector("#input").addEventListener("change", textInputHandler);
+
 console.log("hello script js");
 
-let currentInput;
-const myEmoji = [];
+
+
 
 var inputHappened = function(currentInput) {
-  document.getElementById('input').value = "";
+  const myEmoji = [];
+  document.getElementById("input").value = "";
+      console.log("myEmojiArrAfterLoop",myEmoji)
+
   for (let i = 0; i < currentInput; i++) {
-    myEmoji[i] = "🦠";
-    myEmoji.push();
-  }
+  myEmoji[i] = "🦠";
+      }
+      
+  console.log("myEmojiArrAfterLoop",myEmoji)
+
   stuffToDisplay = myEmoji;
+
   console.log(currentInput !== 0);
   if (currentInput !== 0) {
     let newParagraph = document.createElement("p");
     newParagraph.innerHTML = stuffToDisplay;
+    newParagraph.setAttribute("class", "emoji");
     document.body.appendChild(newParagraph);
   }
+
 };
 
-var display = function(stuffToDisplay) {
-// your DOM manipulation code here
+const clear= function(event){
+  console.log(document.querySelectorAll(".emoji"));
+  document.querySelector('body').textContent = "";
+}
+document.querySelector("#clear").addEventListener("click",clear);
 
+var display = function(stuffToDisplay) {
+  const stuff = function() {
+    console.log("sdsds");
+  };
+
+  // your DOM manipulation code here
 };
