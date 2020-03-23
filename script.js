2751fcd3c92030645e790845bcdc1f82a9d59cfa
travisenquiry <travisenquiry@gmail.com>
2020-03-23 21:36:23 +0800

    completed part 1

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display(currentInput);
+
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+  var pineappleRow = document.createElement("p");
+  var pineappleAmount = "";
+  pineappleRow.className = "pineapple";
+  if(stuffToDisplay === "clear"){
+    var i = 0
+    var clearPineapple = document.getElementsByClassName("pineapple");
+    while(i < clearPineapple.length){
+      clearPineapple[i].parentNode.removeChild(clearPineapple[i]);
+    }
+  }
+  var value = parseInt(stuffToDisplay);
+  for (var i = 0; i < value; i++){
+    pineappleAmount = pineappleAmount + "🍍";
+  }
+  pineappleRow.innerHTML = pineappleAmount;
+  output.innerHTML = pineappleAmount;
+  document.body.appendChild(pineappleRow);
+  document.getElementById("input").value = "";
+
 };
