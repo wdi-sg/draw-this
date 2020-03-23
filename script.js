4ab2fae07f2e6491575a8ce4b2b558fd5dc84b69
Rachelik <rachel.ik.87@gmail.com>
2020-03-23 23:39:42 +0800

    Part 2 Done

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 console.log("hello script js");
-var threePineapple = "🍍🍍🍍";
-var twoPineapple = "🍍🍍";
 
 var resetInput = function() {
     input.value = "";
-}
+};
+
+var output = document.querySelector('#output');
 
 var resetOutput = function() {
-    var output = document.querySelector('#output')
+    // var output = document.querySelector('#output');
     output.innerHTML = "";
+};
+
+var makePineappleArray = function() {
+    var pineappleText = output.innerHTML;
+    pineappleArray = pineappleText.split('<br>');
+    pineappleArray.splice(1,1);
+    output.innerHTML = pineappleArray[0];
 }
+
+
 
 var inputHappened = function(currentInput){
   console.log(currentInput);
@@ -27,10 +36,13 @@ var inputHappened = function(currentInput){
   if (currentInput.toLowerCase() === 'clear') {
     resetOutput();
   };
+
+  if (currentInput.toLowerCase() === 'clear 2') {
+    makePineappleArray();
+  }
 };
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-    var output = document.querySelector('#output')
     output.innerHTML += stuffToDisplay;
 };
