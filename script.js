80c11a8205ec3da31cfa63c56be64b813c29fd7a
Rachelik <rachel.ik.87@gmail.com>
2020-03-23 23:14:23 +0800

    part 1 done

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ console.log("hello script js");
 var threePineapple = "🍍🍍🍍";
 var twoPineapple = "🍍🍍";
 
+var resetInput = function() {
+    input.value = "";
+}
+
+var resetOutput = function() {
+    var output = document.querySelector('#output')
+    output.innerHTML = "";
+}
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
+  console.log(currentInput);
+  resetInput();
   if (parseInt(currentInput) === 3) {
     display("🍍🍍🍍");
-  } else if (parseInt(currentInput) === 2) {
-    return display(pineappleTwoRow);
-  }
+  };
+
+  resetInput();
+
+  if (parseInt(currentInput) === 2) {
+    display("<br>") + display("🍍🍍");
+  };
+
+  if (currentInput.toLowerCase() === 'clear') {
+    resetOutput();
+  };
 };
 
-
-// var display = function(stuffToDisplay){
-//   // your DOM manipulation code here
-//   var output = document.querySelector('#output')
-//     output.innerHTML = stuffToDisplay;
-//     var document.querySelector()
-// };
+var display = function(stuffToDisplay){
+  // your DOM manipulation code here
+    var output = document.querySelector('#output')
+    output.innerHTML += stuffToDisplay;
+};
