a8fddf0b70459a6c36fb3bbc0cfca3e8ec788819
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-23 21:55:10 +0800

    Initial Pineapple and clear output functionality

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  if (currentInput.includes('clear')){
+    clearOutput();
+  } else {
+    display( parseInt(currentInput) );
+  }
 };
 
-var display = function(stuffToDisplay){
+var clearOutput = function(){
+    var output = document.querySelector('#output');
+    output.innerHTML = "";
+}
+
+//displaying pineapples
+var display = function(numberOfPineapples){
   // your DOM manipulation code here
-  
+  var output = document.querySelector('#output');
+  var pineapple = document.createElement("p");
+  pineapple.innerHTML = "🍍".repeat(numberOfPineapples);
+
+  output.appendChild(pineapple);
+
 };
