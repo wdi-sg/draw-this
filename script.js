28a672571136d6c32f69ebdc5cb3de47ab482741
chelsejw <chelsejw@gmail.com>
2020-03-24 07:21:37 +0800

    add choose emoji function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var rowCount = 0;
 var output = document.getElementById(`output`);
+var addedEmoji = false;
+var emoji;
+
+output.textContent = "To get started, choose an emoji to display."
 
 function clearInput() {
     document.getElementById(`input`).value = ``;
@@ -9,7 +13,7 @@ function clearRow(rowNo) {
     //get the array of all elements with class "row"
     var arrayOfRows = document.querySelectorAll(".row")
     //target the row number (-1 to account for element at index 0)
-    var targetRow = arrayOfRows[rowNo-1];
+    var targetRow = arrayOfRows[rowNo - 1];
     //remove target row
     return output.removeChild(targetRow);
 }
@@ -17,7 +21,7 @@ function clearRow(rowNo) {
 function addRow(length) {
     var arrayContent = [];
     for (var i = 0; i < length; i++) {
-        arrayContent.push("🍍");
+        arrayContent.push(emoji);
     };
     var newRow = document.createElement(`p`);
     newRow.textContent = arrayContent.join(``);
@@ -26,32 +30,40 @@ function addRow(length) {
 }
 
 var inputHappened = function(currentInput) {
-    rowCount++;
-    clearInput();
+  clearInput();
 
-    //If input is just "clear"
-    if (currentInput === `clear`) {
-        return output.textContent = ``;
-
-    //If input is not "clear" but longer than one character
-    } else if (currentInput.length > 1) {
-      var instructions = currentInput.split(" ");
-
-      //If the first word is clear, assume second word is number and clear that row.
-      if (instructions[0]===`clear`) {
-        var rowToClear = parseInt(instructions[1]);
-        clearRow(rowToClear);
-
-      //If the first word is not clear, assume it's all numbers.
-      } else {
-        for (var i=0; i < instructions.length; i++) {
-          display(addRow(parseInt(instructions[i])));
-        }
-      }
-    //If input is just one character, assume it's a number and add row.
+    if (!addedEmoji) {
+        addedEmoji = true;
+        emoji = currentInput
+        output.textContent = "";
     } else {
-          var newRow = addRow(parseInt(currentInput));
-          return display(newRow);
+
+        rowCount++;
+
+        //If input is just "clear"
+        if (currentInput === `clear`) {
+            return output.textContent = ``;
+
+            //If input is not "clear" but longer than one character
+        } else if (currentInput.length > 1) {
+            var instructions = currentInput.split(" ");
+
+            //If the first word is clear, assume second word is number and clear that row.
+            if (instructions[0] === `clear`) {
+                var rowToClear = parseInt(instructions[1]);
+                clearRow(rowToClear);
+
+                //If the first word is not clear, assume it's all numbers.
+            } else {
+                for (var i = 0; i < instructions.length; i++) {
+                    display(addRow(parseInt(instructions[i])));
+                }
+            }
+            //If input is just one character, assume it's a number and add row.
+        } else {
+            var newRow = addRow(parseInt(currentInput));
+            return display(newRow);
+        }
     }
 }
 
