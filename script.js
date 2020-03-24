ff0664cba86f760f32be6f10ce42a1534dd29172
kokchuantan <kokchuan1996@gmail.com>
2020-03-24 21:43:18 +0800

    added clear 2 function

--- script.js
+++ script.js
@@ -2,37 +2,57 @@ console.log("hello script js");
 
 var inputArray = [];
 var bird = '🐦';
-var input , x;
+var input, x, input1, input2;
 var birdOutput = '';
+var outputArray = [];
+var lastOutput, outputLength;
+var clearLine;
+var count = 1;
 
-var inputHappened = function(currentInput){
-  var input = currentInput;
-  if(!isNaN(parseInt(input))){
-    inputArray.push(input);
-    console.log( currentInput );
-    //display( "WOW SOMETHING HAPPENED" );
+var inputHappened = function (currentInput) {
+    inputArray = currentInput.split(" ");
+    console.log(inputArray);
+    if (inputArray.length === 1) {
+        if (!isNaN(parseInt(inputArray[0]))) {
+            inputArray.push(inputArray[0]);
+            console.log(currentInput);
+            //display( "WOW SOMETHING HAPPENED" );
 
-    for(x = 0 ; x < input ; x++){
-      birdOutput += bird;
-      console.log(birdOutput);
+            for (x = 0; x < inputArray[0]; x++) {
+                birdOutput += bird;
+                console.log(birdOutput);
+            }
+            outputArray.push(birdOutput);
+            console.log(outputArray);
+            outputLength = outputArray.length - 1;
+            lastOutput = outputArray[outputLength]
+            display();
+        } else if (inputArray[0] === 'clear') {
+            clear();
+        } else {
+            document.querySelector('#output').innerText = 'Invalid Character';
+        }
+    } else if (inputArray.length === 2) {
+        {
+            clearLine = parseInt(inputArray[1]);
+            var list = document.getElementById('output');
+            list.removeChild(list.childNodes[clearLine - 1])
+            console.log(clearLine);
+        }
+    } else {
+        document.querySelector('#output').innerText = 'Invalid Character';
     }
-    display();
-  }
-  else if (input === 'clear'){
-    clear();
-  }
-  else{
-    document.querySelector('#output').innerText ='Invalid Character';
-  }
 };
 
-var display = function(){
-  // your DOM manipulation code here
-  var newPara = document.createElement('p');
-    newPara.innerText = birdOutput;
+var display = function () {
+    // your DOM manipulation code here
+    var newPara = document.createElement('p');
+    newPara.id = 'a' + count;
+    count++
+    newPara.innerText = lastOutput;
     document.getElementById('output').appendChild(newPara);
     birdOutput = '';
 };
-var clear = function(){
-  document.querySelector('#output').innerText ='';
+var clear = function () {
+    document.querySelector('#output').innerText = '';
 }
