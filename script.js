8e21a56ba6f60383204f569475c0ef82e61742cb
Denny Koh <denny.koh.kaichao@gmail.com>
2020-03-24 09:58:39 +0800

    first commit after finishing part 1

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-console.log("hello script js");
+console.log("DRAW THIS THROUGH DOM MANIPULATION!");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+function clearInput() {
+  document.getElementById("input").value = "";
+}
+
+var fruit = "🍍";
+var storeInput = 0;
+var showOutput = "";
+
+var inputHappened = function(currentInput) {
+  var storeInput = currentInput;
+  clearInput();
+  console.log(storeInput);
+  if (!isNaN(parseInt(storeInput)) && storeInput < 10) {
+    for (i = 0; i < storeInput; i++) {
+      showOutput = showOutput + fruit;
+      console.log(showOutput);
+    }
+    var display = document.createElement("p");
+    display.innerText = showOutput;
+    document.getElementById("output").appendChild(display);
+    showOutput = "";
+  } else if (!isNaN(parseInt(storeInput)) && storeInput > 9) {
+    var display = document.createElement("p");
+    display.innerText = "Value entered is too big! Please Enter 1 - 9 only";
+    document.getElementById("output").appendChild(display);
+  } else if (storeInput === "clear") {
+    console.log("hello this is working!");
+    document.querySelector("#output").textContent = "";
+  } else {
+    document.querySelector("#output").textContent =
+      "Please enter only numbers!";
+  }
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+//Part 1 completed//
