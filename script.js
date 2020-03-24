c0834cdd57583725f8c21c1ac9d57e380e0aee02
“Marcelchia” <marchl.chiayl@gmail.com>
2020-03-24 18:27:35 +0800

    saved

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-console.log("hello script js");
-
+/*console.log("hello script js");
 var inputHappened = function(currentInput){
   console.log( currentInput );
   display( "WOW SOMETHING HAPPENED" );
+};*/
+
+//variables
+var inputNumArray = [];
+var emoji = "🍍";
+var emojiArray = [];
+debugger;
+//DOM manipulation for display
+var display = function() {
+  // your DOM manipulation code here
+  clearAllDOM();
+  for (let i = 0; i < emojiArray.length; i++) {
+    var p = document.createElement("p");
+    p.textContent = emojiArray[i];
+    var output = document.getElementById("output");
+    output.appendChild(p);
+  }
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
+//creates emoji array for printing
+var emojiArrayPrint = function() {
+  emojiArray = inputNumArray.map(element => {
+    var toPrint = "";
+    for (var i = 0; i < element; i++) {
+      toPrint = toPrint + emoji;
+    }
+    return toPrint;
+  });
+};
+
+//clear all DOM
+var clearAllDOM = function() {
+  var parent = document.getElementById("output");
+  parent.innerHTML = "";
 };
