99457db45cb8b1080658904c16d7f68ddafd49e6
marctanyh <marctanyh@gmail.com>
2020-03-24 08:33:18 +0800

    Marcus draw-this Part 1
    .js file added.

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 console.log("hello script js");
 
+var vibe = "🙃";
+
+var output = document.querySelector("#output");
+
+var totalVibe = " ";
+
+var additionalVibe = 0;
+
+var clearInput = function () {
+
+  document.querySelector("#input").value = "";
+
+}
+
 var inputHappened = function(currentInput){
+
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+  display(currentInput);
+
+  if ( currentInput === "clear" ){
+
+    document.querySelector("#output").innerHTML = "";
+  }
+
+};
+
+var madVibes = function (vibeRows) {
+
 };
 
 var display = function(stuffToDisplay){
+
   // your DOM manipulation code here
-  
+
+  var vibeRows = document.createElement("p");
+
+  for ( var i = 0; i < stuffToDisplay; i++ ){
+
+    totalVibe += vibe;
+
+  }
+
+  vibeRows.innerText = (totalVibe);
+
+  document.querySelector("#output").appendChild(vibeRows);
+
+  totalVibe = "";
+
 };
