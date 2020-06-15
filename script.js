d2ce7e176f4c76d664f1a2caf35253758b84d368
Maaruf199 <maaruf_kassim@live.com.sg>
2020-06-16 06:41:05 +0800

    Maaruf

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+    var display = function(number)
+    {
+        var p = document.createElement("p");
+        p.setAttribute("class","newP");
+        p.textContent = emoji.repeat(number);
+        document.body.appendChild(p);
+        currentRow ++;
+    }
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+var currentRow = 1
+var emoji = "🍍";
+
+
+var inputHappened = function(currentInput){
+if (!isNaN(parseInt(currentInput))) {
+        display(parseInt(currentInput));
+
+    } else if (currentInput === "clear") {
+        var output = document.body.getElementsByTagName("p");
+            var o = document.body.getElementsByTagName("p")[1];
+            document.body.removeChild(o);
+            var o2 = document.body.getElementsByTagName("p")[1];
+            document.body.removeChild(o2);
+    } else if (currentInput === "clear 2") {
+        var output = document.body.getElementsByTagName("p");
+            var o = document.body.getElementsByTagName("p")[2];
+            document.body.removeChild(o);
+    }
+}
