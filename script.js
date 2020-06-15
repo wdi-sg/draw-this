59cdb659363d7ffa41ff2866c970d13d89d041bf
Isabella <65014479+izzycsy@users.noreply.github.com>
2020-06-15 23:16:23 +0800

    Futher 1.1

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 console.log("hello script js");
+var number;
+var count = "0";
+var pancake = "🥞";
+var clearPancake = " ";
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  //create a p tag with id of addPancake
+  var addPancake = document.createElement("p");
+  number = parseInt(currentInput);
+  document.querySelector("#input").value = "";
+  for(count = 0; count < number; count++){
+    clearPancake = clearPancake + pancake;
+  }
+    console.log(clearPancake);
+  //append pancake to document body
+    addPancake.innerHTML = clearPancake;
+    document.getElementById("output").appendChild(addPancake);
+    clearPancake = " ";
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+function myFunction() {
+  var stackedPancake = document.getElementById("output");
+  stackedPancake.remove();
+}
+
