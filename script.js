5bc0f53b5870a4d3956c88dfb77c46c9bc58629c
yonghan <wangyh.92@gmail.com>
2020-06-15 17:02:03 +0800

    firstVersion

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-console.log("hello script js");
+var flagHolder = [];
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+  var flagLength = parseInt(currentInput);
+
+  if(currentInput.toLowerCase() === "clear"){
+    document.querySelector("#output").innerHTML="";
+  }
+
+  for(i=0;i<flagLength;i++){
+    flagHolder.push("🏁");
+  }
+
+  display();
+
+  //clear the input
+  document.querySelector("#input").value = "";
 };
+
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+  var output = document.querySelector("#output");
+  var createP = document.createElement("p");
+  createP.innerHTML = (flagHolder.join(""));
+  output.appendChild(createP);
+  flagHolder = [];
 };
