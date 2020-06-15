84e970347e5b5146fcd975c5d51443082b96f25c
joeyqlim <joeyqlim@gmail.com>
2020-06-15 17:00:00 +0800

    Allow user to add two rows and choose character or emoji to draw with.

--- script.js
+++ script.js
@@ -4,41 +4,66 @@ const userInput = document.getElementById('user-input');
 const firstResult = document.getElementById('first-row');
 const secondResult = document.getElementById('second-row');
 
-const drawing = [];
+var drawing = [];
 var inputCount = 0;
 
-// FIRST VERSION
+const char = prompt(`Type the character or emoji you'd like to draw with.`);
 
 // submit function
 function submit() {
   var input = userInput.value;
-  // first input
+  var splitInput = input.split(" ");
+  var firstInput = splitInput[0];
+  var secondInput = splitInput[1];
+
+  // one input only
+  if (splitInput.length < 2) {
+    if (inputCount === 0) {
+      while (firstInput > 0) {
+        drawing.push(`${char}`);
+        firstInput--;
+      }
+      firstResult.innerText = drawing.join('');
+      inputCount++;
+      userInput.value = ""
+    } 
+    else if (inputCount === 1) {
+      while (firstInput > 0) {
+        drawing.push(`${char}`);
+        firstInput--;
+      }
+      inputCount++;
+      secondResult.innerText = drawing.join('');
+      userInput.value = "";
+    }
+  }
+
+  // two inputs
   if (inputCount === 0) {
-    while (input > 0) {
-      drawing.push('🐨');
-      input--;
-      userInput.value = ""
+    while (firstInput > 0) {
+      drawing.push(`${char}`);
+      firstInput--;
     }
-    inputCount++;
     firstResult.innerText = drawing.join('');
-  } else if (inputCount === 1) {
-    while (input > 0) {
-      drawing.push('🐨');
-      input--;
-      userInput.value = ""
+    drawing = [];
+    while (secondInput > 0) {
+      drawing.push(`${char}`);
+      secondInput--;
     }
-    inputCount++;
     secondResult.innerText = drawing.join('');
-  } else if (userInput.value === "clear") {
+    userInput.value = ""
+    inputCount++;
+  } 
+  // clear 
+    if (userInput.value === "clear") {
       inputCount = 0;
       firstResult.innerText = '';
       secondResult.innerText = '';
+      userInput.value = "";
+    } else if (userInput.value === "clear 2") {
+      inputCount = 0;
+      secondResult.innerText = '';
       userInput.value = ""
-    // SECOND VERSION: clear second row 
-  } else if (userInput.value === "clear 2") {
-    inputCount = 0;
-    secondResult.innerText = '';
-    userInput.value = ""
   }
 }
 
