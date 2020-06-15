20717c6bfbc5d867e203abae2fd409ed7b0be75f
Nur Amirah Sham <nuramirahsham@gmail.com>
2020-06-16 00:25:54 +0800

    added script for draw this program

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 console.log("hello script js");
+document.getElementById("output").innerText = "Draw with emoji"
 
+//variables and functions
+var emoji = "";
+
+function clearElements() {
+    document.querySelector("#input").value = "";
+    document.getElementById("output").innerText = "";
+}
+
+function splitInput(x) {
+    var array = x.split(" ");
+    return array;
+}
+
+//DOM Manipulation code goes here
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+
+    var array = splitInput(currentInput);
+    var arrayNumber = parseInt(array[1]);
+
+    if (array.length === 1 && array[0] === "clear") {
+        clearElements();
+    } else if (array[0] === "clear" && array.length > 1) {
+        document.getElementById("output").splice(arrayNumber, 1);
+    } else if (typeof(array[0]) !== 'number' && typeof(array[1])!== 'number') {
+        for (var i = 0; i < arrayNumber; i++) {
+        display(array[0]);
+        }
+    } else {
+        display(currentInput);
+    }
+}
 
 var display = function(stuffToDisplay){
-  // your DOM manipulation code here
-  
-};
+    var pTag = document.createElement('p');
+
+    for (var i = 0; i < stuffToDisplay; i++) {
+    emoji += "🗿";
+    }
+
+    pTag.classList.add("emoji");
+    pTag.textContent = emoji;
+
+    var output = document.getElementById("output");
+        output.appendChild(pTag);
+    };
