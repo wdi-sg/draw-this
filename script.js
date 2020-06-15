cfcd7185b85f138da970206ec2b29f6eada635cb
siu-sing <lai.siusing@gmail.com>
2020-06-15 17:12:50 +0800

    added first version

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 console.log("hello script js");
 
+var px = "🍍";
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  display(currentInput);
 };
+
+var display="";
 
 var display = function(stuffToDisplay){
   // your DOM manipulation code here
-  
+    document.getElementById("output").appendChild(drawRow(stuffToDisplay));
 };
+
+
+//add to output child
+
+//return element with rows drawn.
+var drawRow = function(num){
+    let p = document.createElement("p")
+    for(let i = 0; i < num; i++){
+        p.innerHTML+=px;
+    }
+    return p
+}
