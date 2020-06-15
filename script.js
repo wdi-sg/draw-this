3ee8f74c90eaf893fad9232a012d3ddabd2e32b9
akjw <aseykoh@gmail.com>
2020-06-15 21:58:26 +0800

    add multiple rows

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 console.log("hello script js");
 //Instructions:
 var howTo = document.createElement('h2');
-howTo.innerText = 'Valid inputs: \n' + '"clear" will clear display \n' + '"3" will print emoji by 3 times \n' + '"clear 2" will clear 2nd row';
+howTo.innerText = 'Valid inputs: \n' + '"3" will print emoji by 3 times \n' + '"clear 2" will clear 2nd row';
 var textBox = document.querySelector('#input');
 var body = document.querySelector('body');
 body.insertBefore(howTo, textBox);
+
+//Clear All Button
+var button = document.createElement('button');
+button.innerText = 'clear';
+button.onclick = function (){clearDisplay();}
+body.appendChild(button);
 
 //Global variables
 var order;
@@ -14,33 +20,44 @@ var printThis;
 
 var inputHappened = function(currentInput){
   var userArray = currentInput.split(' ');
-  //if there's only one element in the array (i.e. no spaces in user input)
-    if (userArray.length ===1){
-      if (currentInput === 'clear'){
-        console.log('please clear');
-        clearDisplay();
-      }
+  //if there's only one element in array (i.e. no spaces in user input) & not number
+    if (userArray.length === 1 && isNaN(currentInput)){
+      display('invalid input!')
+    }
+  //if there's only one element in the array & it's a number
+    if (userArray.length === 1){
       number = parseInt(userArray[0]);
       printThis = printEmojis(number);
       display(printThis);
     } else {
-      if (userArray[0]=== 'clear'){
+      if (userArray[0] === 'clear'){
         number = parseInt(userArray[1]);
         console.log('clear ' + number + ' row');
         clearRow(number);
       } else {
-        //TBD: print multiple rows
-      }
-    }
+        number = parseInt(userArray[0]);
+        printThis = printEmojis(number);
+        //display(printThis);
+        var rowTimes = parseInt(userArray[1]);
+        display(printThis, rowTimes);
+      } 
+    } 
   }
- 
 
 //Helper Functions
-var display = function(stuffToDisplay){
-  var results = document.createElement('p');
-  results.innerText = stuffToDisplay;
-  document.querySelector('#output').appendChild(results);
-  
+var display = function(stuffToDisplay, numbers){
+  if (numbers > 1){
+    for (var i = 0; i < numbers; i++){
+      var results = document.createElement('p');
+      results.innerText = stuffToDisplay;
+      document.querySelector('#output').appendChild(results);
+    }
+  } 
+  else {
+    var results = document.createElement('p');
+    results.innerText = stuffToDisplay;
+    document.querySelector('#output').appendChild(results);
+  }
 };
 
 var clearInput= function(){
@@ -56,6 +73,25 @@ var clearRow = function(rowNumber){
   var rows = document.querySelectorAll('#output p');
   console.log(rows);
   rows[rowNumber - 1].remove();
+  // Alternate Notation 1: rows.remove(rowNumber -1); <---Why doesn't this notation work?
+  //
+  // Alternate Notation 2: while (rowNumber > 0){
+  //   rows.removeChild(rows.lastChild);
+  //   console.log('removed!');
+  //   numberOfRows--;
+  // }
+}
+
+var printMultiple = function(num, printedRow){
+  for (var i = 0; i < num; i++){
+    var multiplePrint = printedRow + multiplePrint;
+  }
+  return multiplePrint
+  // var newNum = num -1;
+  // while (newNum > 0) {
+  //   return printedRow;
+  //   newNum--;
+  // }
 }
 
 var printEmojis = function(num){
