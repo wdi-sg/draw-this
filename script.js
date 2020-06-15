e5450cf0d2f6059f499f47b496244dae6506edbc
elisaes <alisa260785@gmail.com>
2020-06-15 17:45:38 +0800

    first part

--- script.js
+++ script.js
@@ -1,11 +1,60 @@
-console.log("hello script js");
+const rowsButton = document.querySelector("#rows");
+const triButton = document.querySelector("#triangles");
+let emojiArr = [];
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+const makeRows = (e) => {
+  const content = document.querySelector(".content");
+  content.innerHTML = "";
+
+  const title = document.createElement("h1");
+  title.innerText = "YOU ARE IN THE ROWS MODE";
+  content.appendChild(title);
+
+  const description = document.createElement("p");
+  description.innerText =
+    "Please insert the numbers of rows that you want to display, and press enter";
+  content.appendChild(description);
+
+  const inputRows = document.createElement("input");
+  inputRows.id = "inputRows";
+
+  content.appendChild(inputRows);
+  inputRows.addEventListener("change", inputHappened);
+
+  const rowsDisplay = document.createElement("div");
+  rowsDisplay.className = "display";
+  document.querySelector("body").appendChild(rowsDisplay);
+
+  const clearButton = document.createElement('button');
+  clearButton.class = "clearButton";
+  clearButton.innerText = "clear";
+  clearButton.addEventListener("click", clear)
+  content.appendChild(clearButton);
 };
 
-var display = function(stuffToDisplay){
-  // your DOM manipulation code here
+const inputHappened = (e) => {
+  emojiArr = [];
+
+  const rows = document.createElement('div');
+  rows.className = 'rowsEmoji';
+  const inputValue = e.target.value;
+  console.log("inputValue", inputValue);
   
+  for (let i = 0; i < inputValue; i++) {
+    emojiArr.push("🍓");
+  }
+  console.log("emojiArr", emojiArr);
+  rows.innerHTML = emojiArr;
+  
+ 
+  document.querySelector(".display").appendChild(rows);
 };
+
+const clear = () =>{
+
+document.querySelector(".display").innerHTML = "";
+
+}
+
+rowsButton.addEventListener("click", makeRows);
