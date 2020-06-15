d8133108d3a18792c7d50316a9b82dc3a4b7eba0
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-06-15 21:35:02 +0800

    First Version

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
+function inputHappened(currentInput) {
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
 
-var display = function(stuffToDisplay){
+  if (currentInput == "clear") {
+
+    clearOuput();
+
+  } else {
+
+    display(checkInput(currentInput));
+
+  }
+
+}
+
+function display(stuffToDisplay) {
   // your DOM manipulation code here
   
-};
+  var newP = document.createElement('p');
+  newP.textContent = emoji(stuffToDisplay);
+  document.querySelector('#output').appendChild(newP);
+  
+}
+
+function checkInput(input) {
+
+  if (isNaN(input)) {
+    
+    //return error message
+    return "Please key in a valid number!";
+
+  } else {
+
+    var tempNum = parseInt(input);
+    return tempNum;
+
+  }
+
+}
+
+function emoji(num) {
+
+  var emoji = "💀";
+
+  return emoji.repeat(num);
+
+}
+
+function clearOuput() {
+  var output = document.querySelector('#output');
+  output.innerHTML = "";
+}
